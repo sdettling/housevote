6cfbe798c4af24bc695d5f36a48025ae614bec24
Stephen Dettling <sdettling@gmail.com>
2012-04-21 11:06:51 -0400

    take out number to currency

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,9 +1,6 @@
 class HousesController < ApplicationController
   def index
     @houses = House.all
-    @houses.each { |house|
-      house.price = number_to_currency(house.price)
-     } 
     respond_to do |format|
       format.json { render json: @houses }
     end
