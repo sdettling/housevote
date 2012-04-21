9e9c0287b5b6c448cf15c62dcc89311bf37dd393
Stephen Dettling <sdettling@gmail.com>
2012-04-21 11:20:33 -0400

    updated seeds

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,9 +1,6 @@
 class HousesController < ApplicationController
   def index
     @houses = House.all
-    @houses.each_with_index do |house, index|
-      house[price] = number_to_currency(house[price])
-    end
     respond_to do |format|
       format.json { render json: @houses }
     end
