2abfad900cd360d58f311587d0fad5ff3105c38b
Stephen Dettling <sdettling@gmail.com>
2012-04-21 11:14:14 -0400

    testing array manipulation

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,9 @@
 class HousesController < ApplicationController
   def index
     @houses = House.all
+    @houses.each_with_index do |house, index|
+      @house[index][price] = number_to_currency(house[price])
+    end
     respond_to do |format|
       format.json { render json: @houses }
     end
