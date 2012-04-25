9cfc9d83b708e35e534537d8fdda6332135eb62f
Stephen Dettling <sdettling@gmail.com>
2012-04-24 22:10:37 -0400

    removing lake house

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,7 @@ houses = [
   {name: 'Woodstock', slug: 'woodstock', url: 'http://www.homeaway.com/vacation-rental/p122468', price: '$2,000', description: 'Friday the 13th Part IV: Jason\'s house with a pool... to drown us!'},
   {name: 'Breathtaking Views', slug: 'breathtaking', url: 'http://www.homeaway.com/vacation-rental/p265490', price: '$1,754.50', description: 'Friday the 13th Part V: Jason\'s Cabin on a Cliff... to throw you off of!'},
   {name: 'Sassy Yellow House', slug: 'sassy', url: 'http://www.homeaway.com/vacation-rental/p157483', price: '$950', description: 'Friday the 13th Part VI: Jason\'s Victorian house... It\'s time for tea and death!'},
-  {name: 'Fine Farmhouse', slug: 'farmhouse', url: 'http://www.homeaway.com/vacation-rental/p309483', price: '$1,035', description: 'Friday the 13th Part VII: Jason\'s Renovated farmhouse... of mild annoyance!'},
-  {name: 'Pond and Pool', slug: 'pool', url: 'http://www.homeaway.com/vacation-rental/p168720', price: '$2,650', description: 'Friday the 13th Part VIII: Revenge of Camp Crystal Lake'}
+  {name: 'Fine Farmhouse', slug: 'farmhouse', url: 'http://www.homeaway.com/vacation-rental/p309483', price: '$1,035', description: 'Friday the 13th Part VII: Jason\'s Renovated farmhouse... of mild annoyance!'}
 ]
 
 houses.each {|h| House.create!(*[h])}
